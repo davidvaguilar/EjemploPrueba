8e4ac797b3487ec555364e599f01b3dcfc79fbd1
David Villegas Aguilar <david.villegas.aguilar@gmail.com>
2024-05-15 22:01:12 -0400

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace EjemploPrueba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtRol = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(309, 24);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(244, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "id";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(309, 51);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(243, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "nombre";
+            // 
+            // txtRol
+            // 
+            this.txtRol.Location = new System.Drawing.Point(309, 78);
+            this.txtRol.Name = "txtRol";
+            this.txtRol.Size = new System.Drawing.Size(100, 20);
+            this.txtRol.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(242, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "rol";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(221, 123);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 6;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(318, 123);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 7;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(416, 123);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnActualizar.TabIndex = 8;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 240);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtRol);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtId);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtRol;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnActualizar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjemploPrueba
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            string id = txtId.Text;
+            string nombre = txtNombre.Text;
+            string rol = txtRol.Text;
+    
+            string query = "INSERT INTO dbo.usuarios (id, name, rol) VALUES (" + id + ", '" + nombre + "', '" + rol + "')";
+
+            // MessageBox.Show(query);
+            try{ 
+                SqlConnection conexion = new SqlConnection("server=IQQ042MDALU0206; database=prueba ; integrated security = true");
+                conexion.Open();
+                SqlCommand comando = new SqlCommand(query, conexion);
+                int resultado = comando.ExecuteNonQuery();
+                if( resultado == 0 )
+                {
+                    MessageBox.Show("Registro no insertado");
+                }
+                else
+                {
+                    MessageBox.Show("Registros Agregado exitosamente");
+                }
+                conexion.Close();
+                txtId.Text = "";
+                txtNombre.Text = "";
+                txtRol.Text = "";
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+          
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            string id = txtId.Text.Trim();
+            string query = "DELETE FROM dbo.usuarios WHERE id = " + id;
+
+            if ( id.Length > 0)
+            {
+                try
+                {
+                    SqlConnection conexion = new SqlConnection("server=IQQ042MDALU0206; database=prueba ; integrated security = true");
+                    conexion.Open();
+                    SqlCommand comando = new SqlCommand(query, conexion);
+                    int resultado = comando.ExecuteNonQuery();
+                    if( resultado == 0 )
+                    {
+                        MessageBox.Show("Registro no existe");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Registros Eliminado exitosamente");
+                    }
+                    conexion.Close();
+                    txtId.Text = "";
+                    txtNombre.Text = "";
+                    txtRol.Text = "";
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+            }
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            string id = txtId.Text;    // id a actualizar
+            string nombre = txtNombre.Text;
+            string rol = txtRol.Text;
+
+            string query = "UPDATE dbo.usuarios SET name= '" + nombre + "', rol = '" + rol + "' WHERE id = " + id;
+            //MessageBox.Show(query);
+            if ( id.Length > 0 )
+            {
+                try
+                {
+                    SqlConnection conexion = new SqlConnection("server=IQQ042MDALU0206; database=prueba ; integrated security = true");
+                    conexion.Open();
+                    SqlCommand comando = new SqlCommand(query, conexion);
+                    int resultado = comando.ExecuteNonQuery();
+                    if (resultado == 0)
+                    {
+                        MessageBox.Show("Registro NO Actualizado");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Actualizacion Existosa");
+                    }
+                    conexion.Close();
+                    txtId.Text = "";
+                    txtNombre.Text = "";
+                    txtRol.Text = "";
+                }catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+        }
+    }
+}
